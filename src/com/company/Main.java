3f1e9394c3faebf68be3c46cd5e48f38391ef5a6
Anton <39731038+haniiev@users.noreply.github.com>
2020-03-30 01:19:23 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,13 @@ package com.company;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Person[] people = new Person[5];
-		for (int i = 0; i < 5; i++) {
-			people[i] = Person.input();
-		}
+    public static void main(String[] args) {
+        Employee employee1 = new Employee( "r1",  1,  2);
+        Employee employee2 = new Employee( "r2",  9,  7);
+        Employee employee3 = new Employee( "r3",  10,  20);
+        System.out.println(employee1.getSalary());
 
-		for (Person person : people) {
-			System.out.println(person.output());
-		}
 
-	}
+
+    }
 }
